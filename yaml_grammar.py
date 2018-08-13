3935840dcbefe39e5e08e803c25e51297d527c6a
Daniel Axtens <dja@axtens.net>
2018-08-13 12:39:07 +1000

    fixed bug 1 in my tree
    Signed-off-by: Daniel Axtens <dja@axtens.net>

--- yaml_grammar.py
+++ yaml_grammar.py
@@ -212,8 +212,6 @@ NDef("ipv6_route",
 NDef("route_common",
      Opt("          on-link: true\n"),
      Opt("          metric: ", UInt(min=0, max=65535, odds=[]), "\n"),
-     # bug 1: docs say prohibit_ed_, code accepts prohibit (no ed)
-     # don't specify unicast, it's the default
      Opt("          type: ", Or("unreachable", "blackhole", "prohibit"), "\n"),
      Opt("          scope: ", Or("global", "link", "host"), "\n"),
      Opt("          table: ", UInt(min=1, max=65536, odds=[]), "\n")
